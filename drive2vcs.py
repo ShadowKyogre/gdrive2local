ed984baffe71d8c68bb366b8e3348d21c1e0c502
ShadowKyogre <shadowkyogre@aim.com>
2014-07-01 22:59:35 -0700

    Made executable, changed out censors, and added git revisionness

--- drive2vcs.py
+++ drive2vcs.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
+
 from requests_oauthlib import OAuth2Session
 import os
+from dateutil import parser
+from dateutil import tz
 
 """
-https://docs.google.com/document/export?id=CENSORED&revision=???&exportFormat=zip
-https://docs.google.com/document/export?id=CENSORED&revision=???&exportFormat=odt
-https://docs.google.com/spreadsheets/export?id=CENSORED&revision=196&exportFormat=ods
-https://docs.google.com/presentation/export?id=CENSORED&revision=???&exportFormat=odp
-https://docs.google.com/drawings/export?id=CENSORED&revision=???&exportFormat=svg
+https://docs.google.com/document/export?id=C-c-censored!&revision=???&exportFormat=zip
+https://docs.google.com/document/export?id=C-c-censored!&revision=???&exportFormat=odt
+https://docs.google.com/spreadsheets/export?id=C-c-censored!&revision=196&exportFormat=ods
+https://docs.google.com/presentation/export?id=C-c-censored!&revision=???&exportFormat=odp
+https://docs.google.com/drawings/export?id=C-c-censored!&revision=???&exportFormat=svg
 
 blah = {'application/vnd.oasis.opendocument.text':'odt',
 	'text/plain':'txt',
@@ -28,8 +32,8 @@ export_url_tpl="https://docs.google.com/{}/?id={}&revision={}&exportFormat={}"
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-client_id = r'PUTINYOUROWN'
-client_secret = r'PUTINYOUROWN'
+client_id = r'C-c-censored!'
+client_secret = r'C-c-censored!'
 redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
 scope = ['https://www.googleapis.com/auth/drive.readonly']
 
@@ -40,7 +44,7 @@ authorization_url, state = oauth.authorization_url(
 			'https://accounts.google.com/o/oauth2/auth',
 			# access_type and approval_prompt are Google specific extra
 			# parameters.
-			access_type="offline", approval_prompt="force")
+			access_type="offline", approval_prompt="auto")
 
 print('Please go to {} and authorize access.'.format(authorization_url))
 authorization_response = input('Enter the auth response: ')
@@ -52,6 +56,10 @@ token = oauth.fetch_token(
 			client_secret=client_secret)
 
 api_url_tpl = "https://www.googleapis.com/drive/v2{}"
+
+#unoconv --stdout -f fodt
+#libreoffice --headless --convert-to fodt --outdir /tmp /tmp/BoundSequelIdea.odt <- for storage AND git logs
+#libreoffice --headless --convert-to txt:Text --outdir /tmp /tmp/BoundSequelIdea.odt <- for git logs?
 
 for gdrive_id in os.sys.argv[1:]:
 	r = oauth.get(api_url_tpl.format("/files/{}/revisions".format(gdrive_id)))
@@ -70,16 +78,19 @@ for gdrive_id in os.sys.argv[1:]:
 	for rev in r.json()['items']:
 		if rev.get('downloadUrl') is None:
 			gdoc_indicator="[GDOC] {} @ {}"
-			#optfmt=export_format[f['mimeType']]
-			#exportUrl=export_url_tpl.format(optfmt[0],gdrive_id,rev['id'],optfmt[1])
-			#data=oauth.get(rev.get(exportUrl))
+			optfmt=export_format[f['mimeType']]
+			exportUrl=export_url_tpl.format(optfmt[0],gdrive_id,rev['id'],optfmt[1])
+			data=oauth.get(rev.get(exportUrl))
 		else:
 			gdoc_indicator="[FILE] {} @ {}"
-			#data=oauth.get(rev.get('downloadUrl'))
+			data=oauth.get(rev.get('downloadUrl'))
 		print(gdoc_indicator.format(rev['id'], rev['modifiedDate']))
 		
-		"""
-		with open(filename, 'wb') as fd:
+		#"""
+		with open(fname, 'wb') as fd:
 			for chunk in data.iter_content(chunk_size):
 				fd.write(chunk)
-		"""
+			subprocess.call(['git', 'add', fname])
+			revdate=parser.parse(rev['modifiedDate']).astimezone(tz.tzlocal())
+			subprocess.call(['git', 'commit', '--date', revdate.isoformat(), '-m', gdoc_indicator])
+		#"""
